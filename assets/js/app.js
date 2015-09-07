bcd8669d1b0fc05dd0d9fa5a4119b3d776997aeb
David Gonzalez <davidjgonzalez@gmail.com>
2015-09-07 13:57:41 -0400

    #bugfixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,24 @@
-$(function() { 
-	$('body').on('click', '#filters a', function() { 
+$(function() {
+	$('body').on('click', '#filters a', function() {
 		var $this = $(this),
 			$episodes = $('#episode-list .episode-row'),
 			$listTitle = $('#list-title'),
 			tags = $this.data('tags').split(' ');
-		
+
 			$listTitle.text($this.text() + ' Episodes');
-		
-			$episodes.each(function(index) { 
+
+			$episodes.each(function(index) {
 				var $episode = $(this).find('.episode');
-				episodeTags = $episode.data('tags').split(' ');
-				
-				if(containsAny(tags, ['all']) || containsAny(episodeTags, tags)) {
+
+				if (!tags
+						|| (tags.length > 0 && tags[0] === 'all')
+						|| $episode.is('[data-tags*=' + tags + ']')) {
 					$episode.show();
 				} else {
 					$episode.hide();
 				}
 			});
-		
+
 			return false;
 	});
-	
-	
-	function containsAny(arr1, arr2) {
-		for(var i = 0; i < arr1.length; i++) {
-			for(var j = 0; j < arr2.length; j++) {
-				if(arr1[i] === arr2[j]) {
-					return true;
-				}			
-			}
-		}
-		
-		return false;		
-	}
 });
